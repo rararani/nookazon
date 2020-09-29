c9c21230bae53ed26b52f756dfafa63be2801241
Rajvi Rana <rajvirana707@gmail.com>
2020-09-28 22:49:10 -0400

    added react-navigation

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import ShoppingScreen from "./app/screens/ShoppingScreen";
-import CheckoutScreen from "./app/screens/CheckoutScreen";
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  console.log("App executed.");
-  return <CheckoutScreen />;
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
