c9fcf551a417e098ef49097d283afaf3a183a364
Rajvi Rana <rajvirana707@gmail.com>
2020-09-28 22:12:32 -0400

    added a basic shopping screen

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ShoppingScreen from "./app/screens/ShoppingScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  console.log("App executed.");
+  return <ShoppingScreen />;
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Hello World!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
